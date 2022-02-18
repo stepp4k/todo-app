a1de7c5968f2b8e60b10f9051f962e080aa19bbf
stepp4k <94464312+stepp4k@users.noreply.github.com>
2022-02-18 13:03:49 -0500

    Building routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header";
 import Tasks from "./components/Tasks/Tasks";
-
 import MainMenu from './components/MainMenu/MainMenu';
+import Help from "./components/Help/Help";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Header />
       <MainMenu />
       <Tasks />
+      <Help />
     </div>
   );
 }
---
+++ src/components/Help/Help.js
@@ -0,0 +1,11 @@
+import './Help.scss'
+
+export default function Help() {
+    return (
+        <div className='help'>
+            <h1>Help</h1>
+            <p>Here you can make and track your ToDo list</p>
+            <p>Add new tasks by writing a <strong>description</strong>, selecting <strong>status</strong> and clicking the <strong>Add</strong> button</p>
+        </div>
+    )
+}
